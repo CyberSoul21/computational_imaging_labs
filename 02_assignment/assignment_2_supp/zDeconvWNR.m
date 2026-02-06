b18be0a7a08a83cc5aadb7a786c0ee0986497a40
alvaroprovencio <alvaroproven@gmail.com>
2026-02-06 11:28:35 +0100

    Code changes convlucy and exercise 5

--- 02_assignment/assignment_2_supp/zDeconvWNR.m
+++ 02_assignment/assignment_2_supp/zDeconvWNR.m
@@ -4,12 +4,18 @@ function f0 = zDeconvWNR(f, k, C)
     % k: defocus kernel
     % C: sigma^2/A
 
-	[height, width] = size(f);
+	% [height, width] = size(f);
+    [height, width, channel] = size(f); %Exercise 5
+    f0 = zeros([height, width, channel]);
 
 	k = zPSFPad(k, height, width);
 	k = fft2(fftshift(k));
 
-	f0 = abs(ifft2((fft2(f) .* conj(k)) ./ (k .* conj(k) + C)));
+	% f0 = abs(ifft2((fft2(f) .* conj(k)) ./ (k .* conj(k) + C)));
+    %Exercise 5
+    for c = 1:channel
+	    f0(:,:,c) = abs(ifft2((fft2(f(:,:,c)) .* conj(k)) ./ (k .* conj(k) + C)));
+    end
 
 end
 
--- 02_assignment/assignment_2_supp/zDefocused.m
+++ 02_assignment/assignment_2_supp/zDefocused.m
@@ -1,18 +1,27 @@
 function f = zDefocused(f0, k, sigma, flag)
 
-	[height, width] = size(f0);
+	% [height, width] = size(f0);
+	[height, width, channel] = size(f0); %Exercise 5
+    f = zeros([height, width, channel]);
 
     if flag == 1
         % Assume the defocus is circular convolution
 		k = zPSFPad(k, height, width);
 		k = fft2(fftshift(k));
-		f = abs(ifft2(fft2(f0) .* k)) + randn(height, width) * sigma;
-
+        % f = abs(ifft2(fft2(f0) .* k)) + randn(height, width) * sigma;
+        %Exercise 5
+        for c = 1:channel
+		    f(:,:,c) = abs(ifft2(fft2(f0(:,:,c)) .* k)) + randn(height, width) * sigma;
+        end
     else
         % Not make that assumption. But in this case, because of the 
         % boundary effects, the "effective" noise level is very high.
 		k = rot90(k, 2);
-		f = imfilter(f0, k) + randn(height, width) * sigma;
+		% f = imfilter(f0, k) + randn(height, width) * sigma;
+        %Exercise 5
+        for c = 1:channel
+		    f(:,:,c) = imfilter(f0(:,:,c), k) + randn(height, width) * sigma;
+        end
         
     end
 
