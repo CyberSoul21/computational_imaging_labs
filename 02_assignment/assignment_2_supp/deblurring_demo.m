6aa8b81670a667db0e38a1cee709da6be2eb3449
cybersoul21 <wilson.jar21@gmail.com>
2026-02-06 11:54:40 +0100

    loop added

--- 02_assignment/assignment_2_supp/deblurring_demo.m
+++ 02_assignment/assignment_2_supp/deblurring_demo.m
@@ -21,54 +21,62 @@ image = imread('images/penguins.jpg');
 % image = image(:, :, 1); %Comment for color image, Uncomment for gray
 
 % Noise level (Gaussian noise)
-sigma = 0.005;
-% sigma = 0.0005;
+%sigma = 0.005;
+sigma_v = [0.0005, 0.005, 0.010, 0.1, 1, 2];
 
 % Blur size
-blurSize = 7;
-% blurSize = 10;
+%blurSize = 7;
+blurSize_v = [1,7,14];
 
 
 f0 = im2double(image);
 [height, width, channel] = size(f0);
 
-% Prior matrix: 1/f law
-A_star = eMakePrior(height, width) + 0.00000001;
-C = sigma.^2 * height * width ./ A_star;
+for sigma = sigma_v
 
-% Normalization
-temp = fspecial('disk', blurSize);
-flow = max(temp(:));
+    for blurSize = blurSize_v
+        % Prior matrix: 1/f law
+        A_star = eMakePrior(height, width) + 0.00000001;
+        C = sigma.^2 * height * width ./ A_star;
+        
+        % Normalization
+        temp = fspecial('disk', blurSize);
+        flow = max(temp(:));
+        
+        % Calculate effective PSF
+        k1 = im2double(...
+            imresize(aperture, [2*blurSize + 1, 2*blurSize + 1], 'nearest')...
+        );
+        
+        k1 = k1 * (flow / max(k1(:)));
+        
+        % Apply blur
+        f1 = zDefocused(f0, k1, sigma, 0);
+        
+        % Recover
+        psf = zPSFPad(k1, height, width);
+        f0_hat = zDeconvWNR(f1, k1, C); % Wienner with priors
+        % f0_hat = deconvlucy(f1, psf); %Lucy
+        % f0_hat = deconvwnr(f1, psf); %Wienner without priors
+        
+        % Display results
+        figure;
+        
+        subplot_tight(1, 3, 1, 0.0, false)
+        imshow(f0);
+        title('Focused');
+        
+        subplot_tight(1, 3, 2, 0.0, false)
+        imshow(f1);
+        title('Defocused');
+        
+        subplot_tight(1, 3, 3, 0.0, false)
+        imshow(f0_hat);
+        title_s = strcat('Recovered' , ' Sigma: ' , string(sigma), ' BlurSize: ',string(blurSize));
+        title(title_s);
+    end
+end
 
-% Calculate effective PSF
-k1 = im2double(...
-    imresize(aperture, [2*blurSize + 1, 2*blurSize + 1], 'nearest')...
-);
 
-k1 = k1 * (flow / max(k1(:)));
-
-% Apply blur
-f1 = zDefocused(f0, k1, sigma, 0);
-
-% Recover
-psf = zPSFPad(k1, height, width);
-f0_hat = zDeconvWNR(f1, k1, C); % Wienner with priors
-% f0_hat = deconvlucy(f1, psf); %Lucy
-% f0_hat = deconvwnr(f1, psf); %Wienner without priors
-
-% Display results
-figure;
-
-subplot_tight(1, 3, 1, 0.0, false)
-imshow(f0);
-title('Focused');
-
-subplot_tight(1, 3, 2, 0.0, false)
-imshow(f1);
-title('Defocused');
-
-subplot_tight(1, 3, 3, 0.0, false)
-imshow(f0_hat);
-title('Recovered');
 
 
