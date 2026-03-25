70a8fe14c8c5038bf82448c3014b001ed9813a28
alvaroprovencio <alvaroproven@gmail.com>
2026-03-25 13:05:45 +0100

    confocal

--- 04_assignment/NLOS.m
+++ 04_assignment/NLOS.m
@@ -1,28 +1,24 @@
 clear; clc; close all;
-D = load("CI_Lab_NLOS_datasets\Z_d=0.5_l=[1x1]_s=[256x256].mat"); norm_check = 1;
-% D = load("CI_Lab_NLOS_datasets\usaf_d=0.5_l=[1x1]_s=[256x256].mat"); norm_check = 0;
-% D = load("CI_Lab_NLOS_datasets\bunny_d=0.5_l=[1x1]_s=[256x256].mat"); norm_check = 1;
+% D = load("CI_Lab_NLOS_datasets\Z_d=0.5_l=[1x1]_s=[256x256].mat"); norm_check = 0; confoc_check = 0;
+% D = load("CI_Lab_NLOS_datasets\usaf_d=0.5_l=[1x1]_s=[256x256].mat"); norm_check = 1; confoc_check = 0;
+% D = load("CI_Lab_NLOS_datasets\bunny_d=0.5_l=[1x1]_s=[256x256].mat"); norm_check = 0; confoc_check = 0;
+D = load("CI_Lab_NLOS_datasets\bunny_d=0.5_c=[256x256].mat"); norm_check = 1; confoc_check = 1;
+
+% Resolution
+N = 32;
 
 fn = fieldnames(D);
 dataset = D.(fn{1});
 
-% extraer histograma
-H = squeeze(dataset.data);
-
-% First to-do
-[nx,ny,nt] = size(H);
-
-% slices centrales
-x_mid = round(nx/2);
-y_mid = round(ny/2);
-
-Hx = squeeze(H(:,y_mid,:));
-Hy = squeeze(H(x_mid,:,:));
-
-% mejorar visibilidad
-Hx = log(Hx + 1);
-Hy = log(Hy + 1);
-
+% % First to-do
+% H = squeeze(dataset.data);
+% [nx,ny,nt] = size(H);
+% 
+% x_mid = round(nx/2);
+% y_mid = round(ny/2);
+% Hx = squeeze(H(:,y_mid,:));
+% Hy = squeeze(H(x_mid,:,:));
+% 
 % figure
 % imagesc(Hx)
 % axis xy
@@ -36,54 +32,49 @@ Hy = log(Hy + 1);
 % colormap hot
 % colorbar
 % title('y-t slice')
-
-% End first to-do
+% % End first to-do
 
 
 laserPos = dataset.laserPositions;
 spadPos  = dataset.spadPositions;
-% laserOrigin = dataset.laserOrigin;
-% spadOrigin  = dataset.spadOrigin;
 laserOrigin = dataset.laserOrigin(:)';
 spadOrigin  = dataset.spadOrigin(:)';
 deltaT = dataset.deltaT;
 t0 = dataset.t0;
-% volCenter = dataset.volumePosition;
 volCenter = dataset.volumePosition(:)';
 volSize = dataset.volumeSize;
+H = dataset.data;
 
 if isscalar(volSize)
     volSize = [volSize volSize volSize];
 end
 
-% Resolution
-N = 32;
 
-% Histograma (NO squeeze)
-H = dataset.data;
-[lu,lv,su,sv,nt] = size(H);
-
+% Preprocess spad to avoid loops
+disp('preprocess spad');
+spadPos_flat = reshape(spadPos, [], 3);
+if confoc_check == 0 % No confocal
+    [lu,lv,su,sv,nt] = size(H);   
+    H_reshaped = reshape(H(1,1,:,:,:), su, sv, nt);
+    xl = squeeze(laserPos)';
+else % Confocal
+    [su,sv,nt] = size(H); %su, sv would be cu, cv in this case   
+    H_reshaped = H;      
+end
 
 % Grid of voxels
 x = linspace(volCenter(1)-volSize(1)/2, volCenter(1)+volSize(1)/2, N);
 y = linspace(volCenter(2)-volSize(2)/2, volCenter(2)+volSize(2)/2, N);
 z = linspace(volCenter(3)-volSize(3)/2, volCenter(3)+volSize(3)/2, N);
-[X,Y,Z] = meshgrid(x,y,z);
+[X,Y,Z] = ndgrid(x,y,z);
 G = zeros(N,N,N);
 
-% Preprocess spad to avoid loops
-disp('preprocess spad');
-Ns = su * sv;
-spadPos_flat = reshape(spadPos, [], 3);   
-H_reshaped = reshape(H(1,1,:,:,:), su, sv, nt);
-
-xl = squeeze(laserPos(1,1,:))';
-if norm_check == 1
-    d1 = norm(laserOrigin - xl);
-    d4 = sqrt(sum((spadPos_flat - spadOrigin).^2, 2));  
-else
+if norm_check == 1 % Normalized
     d1 = 0;
     d4 = 0;
+else % Not Normalized
+    d1 = norm(laserOrigin - xl);
+    d4 = sqrt(sum((spadPos_flat - spadOrigin).^2, 2));  
 end
 
 disp('ini reprojection');
@@ -91,21 +82,26 @@ for ix = 1:N
     for iy = 1:N
         for iz = 1:N
             xv = [X(ix,iy,iz), Y(ix,iy,iz), Z(ix,iy,iz)];
-            d2 = norm(xl - xv);
+            
+            if confoc_check == 0 % No confocal
+                d2 = norm(xl - xv);
+                diff = spadPos_flat - xv;                 
+                d3 = sqrt(sum(diff.^2, 2));               
+                tv = d1 + d2 + d3 + d4;
+            else % Confocal
+                diff = spadPos_flat - xv;
+                d = sqrt(sum(diff.^2, 2));
+                tv = 2*d;
+            end
 
-            diff = spadPos_flat - xv;                 
-            d3 = sqrt(sum(diff.^2, 2));               
-
-            tv = d1 + d2 + d3 + d4;
             tidx = ceil((tv - t0)/deltaT);
-        
             valid = (tidx > 0) & (tidx <= nt);
 
             if any(valid)
                 tidx_valid = tidx(valid);
                 idx_spad = find(valid);
-        
-                [iu, iv] = ind2sub([su, sv], idx_spad);
+
+                [iu, iv] = ind2sub([su, sv], find(valid));
                 ind = sub2ind([su, sv, nt], iu, iv, tidx_valid);
                 G(ix,iy,iz) = sum(H_reshaped(ind));
             else
@@ -118,11 +114,9 @@ end
 disp('end reprojection');
 
 % This accumulates over all depth
-proj = max(G,[],1); %Collapse over plane Y-Z
+proj = max(G,[],2); %Collapse over plane X-Z
 proj = squeeze(proj);
-% proj = log(proj + 1); %Improve visualization
 figure
-% imagesc(proj)
 imagesc(rot90(proj, -1))
 axis xy
 axis image
@@ -138,12 +132,10 @@ title('Volumen 3D')
 f_lap = fspecial3('lap');
 G_lap = imfilter(G, -f_lap, 'symmetric');
 G_lap(G_lap < 0) = 0;
-proj_lap = max(G_lap,[],1);
+proj_lap = max(G_lap,[],2); %Collapse over plane X-Z
 proj_lap = squeeze(proj_lap);
 figure
-% imagesc(proj_lap)
 imagesc(rot90(proj_lap, -1))
-% imagesc(log(proj_lap+1))
 axis xy
 axis image
 colormap hot
@@ -151,18 +143,18 @@ colorbar
 title('Backprojection + Laplacian')
 
 %laplacian Gauss reconstruction
-sigma = 1.0;   % try 0.5, 1, 2
-f_log = fspecial3('log', [5 5 5], sigma);
-G_log = imfilter(G, -f_log, 'symmetric');
-G_log(G_log < 0) = 0;
-proj_log = max(G_log,[],1);
-proj_log = squeeze(proj_log);
-figure
-% imagesc(proj_log)
-imagesc(rot90(proj_log, -1))
-% imagesc(log(proj_log+1))
-axis xy
-axis image
-colormap hot
-colorbar
-title(['Backprojection + LoG (sigma = ', num2str(sigma), ')'])
+sigma_v = [0.5, 1.0, 2.0];   % try 0.5, 1, 2
+for sigma = sigma_v
+    f_log = fspecial3('log', [5 5 5], sigma);
+    G_log = imfilter(G, -f_log, 'symmetric');
+    G_log(G_log < 0) = 0;
+    proj_log = max(G_log,[],2); %Collapse over plane X-Z
+    proj_log = squeeze(proj_log); 
+    figure
+    imagesc(rot90(proj_log, -1))
+    axis xy
+    axis image
+    colormap hot
+    colorbar
+    title(['Backprojection + LoG (sigma = ', num2str(sigma), ')'])
+end
